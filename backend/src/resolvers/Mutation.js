465523e83dd5d1dea1dbf1a4f5a6efef4d2d7124
Jenaro <jen.calvineo@gmail.com>
2019-11-26 20:14:55 -0300

    Added user tu mutation create item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,10 +7,19 @@ const { transport, makeANiceEmail } = require('../mail')
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     //TODO check if they are logged
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to create new items')
+    }
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
+          // This is how to set a relationship between item and user
+          user: {
+            connect: {
+              id: ctx.request.userId,
+            },
+          },
           ...args,
         },
       },
